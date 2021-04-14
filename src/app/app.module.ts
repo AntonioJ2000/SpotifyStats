c9249570f31665825f7167918bc4597d2a6f7ebd
Ciscu24 <ciscu6@gmail.com>
2021-04-14 19:54:59 +0200

    Añadido para grabar audio

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,15 +9,22 @@ import { AppComponent } from './app.component';
 
 import { SuperTabsModule } from '@ionic-super-tabs/angular';
 import { ApiNodeService } from './services/api-node.service';
-import { HTTP } from '@ionic-native/http/ngx'
+import { HTTP } from '@ionic-native/http/ngx';
+
+import { File } from '@ionic-native/file/ngx';
+import { Media } from '@ionic-native/media/ngx';
 
 @NgModule({
   declarations: [AppComponent],
   entryComponents: [],
   imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule, SuperTabsModule.forRoot()],
-  providers: [HTTP, 
-              ApiNodeService, 
-              { provide: RouteReuseStrategy, useClass: IonicRouteStrategy }],
+  providers: [
+    HTTP, 
+    ApiNodeService,
+    File,
+    Media,
+    { provide: RouteReuseStrategy, useClass: IonicRouteStrategy }
+  ],
   bootstrap: [AppComponent],
 })
 export class AppModule {}
--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
 import { ApiNodeService } from '../services/api-node.service';
+import { File } from '@ionic-native/file/ngx';
+import { Media, MediaObject } from '@ionic-native/media/ngx';
 
 @Component({
   selector: 'app-tab1',
@@ -8,10 +10,32 @@ import { ApiNodeService } from '../services/api-node.service';
 })
 export class Tab1Page {
 
-  constructor(private apiNode:ApiNodeService) {}
+  status:string = "";
+  audioFile:MediaObject;
+
+  constructor(private apiNode:ApiNodeService, private media:Media, private file:File) {}
 
   public async login(){
     const d = await this.apiNode.Authorize();
     console.log(d);
   }
+
+  public grabarAudio(){
+    this.audioFile = this.media.create(this.file.externalRootDirectory+"/audiofile.mp3");
+    this.audioFile.startRecord();
+    this.status = "recording...";
+  }
+
+  public pararAudio(){
+    this.audioFile.stopRecord();
+    this.status = "stopped";
+
+    this.file.readAsDataURL(this.file.externalRootDirectory, "audiofile.mp3").then((base64Audio) => {
+      console.log(base64Audio.substring(23, base64Audio.length));
+      console.log(base64Audio);
+    })
+
+  }
+
+  
 }
